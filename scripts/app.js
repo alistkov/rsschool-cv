af978538b60ffd4a3505e959aa5b6b28434111a4
Anton Listkov <antonlistkov@gmail.com>
2024-07-06 23:24:13 +0200

    fix: update script

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,23 @@
-import { setYearInFooter } from './components/year.js'
-import { scrollToSection } from './components/scroll-to-section.js'
+function scrollToSection() {
+  const anchors = document.querySelectorAll('.js-section-link')
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  })
+}
+
+function setYearInFooter() {
+  const yearContainer = document.querySelector('.js-year')
+  const year = new Date().getFullYear()
+  yearContainer.textContent = year
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   setYearInFooter()
-
   scrollToSection()
 });
--- scripts/components/year.js
+++
@@ -1,5 +0,0 @@
-export function setYearInFooter() {
-  const yearContainer = document.querySelector('.js-year')
-  const year = new Date().getFullYear()
-  yearContainer.textContent = year
-}
